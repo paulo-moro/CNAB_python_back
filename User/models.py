1bc35802c666791cd13c1e2c2f9a7e3c81fd440a
Paulo Henrique Moro dos Santos <paulo.morolol@gmail.com>
2022-10-04 14:48:47 -0300

    feat/created transactions models.

--- User/models.py
+++ User/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
 
 
 class User(AbstractUser):
+    """User model"""
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField()
-    password = models.CharField(max_length=12)
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+    """Models for Transaction"""
+
+    transaction_date = models.DateField()
+    transaction_time = models.TimeField()
+    amount = models.FloatField()
+    CPF = models.IntegerField()
+    card = models.IntegerField()
+    shop_rep = models.CharField(max_length=14)
+    shop_name = models.CharField(max_length=19)
+
+
+class NatureChoices(models.TextChoices):
+    """Nature type Choices"""
+
+    OUTCOME = "Saída"
+    INCOME = "Entrada"
+
+
+class SignalChoices(models.TextChoices):
+    """Signal Choices"""
+
+    PLUS = "+"
+    MINUS = "-"
+
+
+class Type(models.Model):
+    """Type Models"""
+
+    description = models.CharField(max_length=30)
+    nature = models.CharField(max_length=30, choices=NatureChoices.choices)
+    signal = models.CharField(max_length=1, choices=SignalChoices.choices)
+    transaction = models.ForeignKey(
+        "transactions.Transaction",
+        on_delete=models.CASCADE,
+        related_name="transactions",
+    )
+    transaction = models.OneToOneField(
+        "transactions.Transaction", on_delete=models.CASCADE
+    )
